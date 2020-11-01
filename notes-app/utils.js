1633ccf85664ab6de5658f3f11a8863f19f68059
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2020-11-01 12:34:47 -0500

    added modules challenge

---
+++ notes-app/utils.js
@@ -0,0 +1,10 @@
+console.log('util js running');
+
+const name = 'Jimmy Pesto'
+
+const add = (a, b) => {
+    return a + b;
+}
+
+//exports is used to share variables and functions
+module.exports = add
