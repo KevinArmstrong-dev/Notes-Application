61a5ae171464d1f7e4e2a5354b276d3f03aea9bf
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2020-11-01 12:17:53 -0500

    Added append method

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,4 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt','This file was created by node js')
+//fs.writeFileSync('notes.txt','My name is Dante consti');
+fs.appendFileSync('notes.txt',' I\'m a devout  writer of museoms');
