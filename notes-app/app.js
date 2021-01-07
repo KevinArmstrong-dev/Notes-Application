c45f1461ec7d2348b674fec77a400911408e5425
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2021-01-07 16:09:59 -0500

    Added chalk package

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,5 @@
-const validator = require('validator')
+//const validator = require('validator')
+const chalk = require('chalk')
 const notes = require('./notes.js');
 
 // const add = require('./utils.js')
@@ -6,7 +7,9 @@ const notes = require('./notes.js');
 // const sum  = add(4,-2);
 // console.log(sum)
 
-
+console.log(chalk.blue.bgGreen('success'));
+console.log(chalk.red('Fail'));
+//console.log(chalk.purple('Warning'));
 console.log(notes());
-console.log(validator.isEmail('bell@noreply.com'))
+//console.log(validator.isEmail('bell@noreply.com'))
 
