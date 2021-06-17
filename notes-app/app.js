fce1b3abeea98d85650c4b245c5479ce5883ff6d
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2021-06-16 20:34:41 -0400

    Added extra packages for arguments passing

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,11 +3,12 @@ const notes = require('./notes.js');
 
 const command  = process.argv[2]
 if(command === 'add'){
-
+    const msg = chalk.blue.inverse.bold('Note Added!!');
     console.log('adding notes');
+    console.log(msg);
 }else if(command === 'remove'){
     console.log('Remove note')
 }else{
-    console.log('Else note');
+    console.log('No arg provided Else note');
 }
 
