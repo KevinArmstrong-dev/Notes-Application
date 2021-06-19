b776faf87868c121f5689c6a113180c8a83b027c
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2021-06-19 12:11:54 -0400

    Modified body

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,16 +1,63 @@
 const chalk = require('chalk');
-const yargs= require('yargs');
+const { describe, demandOption } = require('yargs');
+const yargs= require('yargs'); //usful for args parsing
 const notes = require('./notes.js');
 
-const command  = process.argv[2]
-console.log(yargs.argv);
-if(command === 'add'){
-    const msg = chalk.blue.inverse.bold(process.argv[3]+ ' Note Added!!');
-    console.log('adding notes');
-    console.log(msg);
-}else if(command === 'remove'){
-    console.log('Remove note')
-}else{
-    console.log('No arg provided Else note');
-}
 
+//customize yargs version
+yargs.version('1.1.0')
+
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder:{
+        title:{
+            describe: 'Note Title',
+            demandOption:true, //required
+            type: 'string'
+        },
+        body:{
+            describe:'Note Content',
+            demandOption:true, //required
+            type:'string'
+        }
+    },
+    handler:function(argv){
+        console.log('Title: '+ argv.title);
+        console.log('Body: '+argv.body);
+    }
+})
+
+//create remove command
+
+yargs.command({
+    command:'remove',
+    describe : 'Remove a note',
+    handler: function(){
+        console.log("Removing a note");
+    }
+})
+
+//create read command
+yargs.command({
+    command:'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log("Reading a note !!");
+    }
+})
+
+//create a list command
+
+yargs.command({
+    command:'list',
+    describe: 'Print the list of notes',
+    handler: function(){
+        console.log('Printing list')
+    }
+})
+
+
+yargs.parse();
+//console.log(yargs.argv);
