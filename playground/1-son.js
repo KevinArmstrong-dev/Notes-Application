66c4db096c92cb4d257173781876e4bd61868c97
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2021-06-19 12:34:31 -0400

    Saving progress

---
+++ playground/1-son.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+// const book = {
+//     title: 'Crime and pubishment',
+//     author:'Fydor'
+// }
+
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync('1-json.json',bookJSON); //write to file
+
+// const dataBuffer = fs.readFileSync('1-json.json');
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+// console.log(data.title);
